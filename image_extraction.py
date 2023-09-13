14860c41c8bdfe4bdaa2d41cbab128de475a8c99
Hans <hansal@ntnu.no>
2023-09-13 14:32:11 +0200

    Made nas run on a video file

--- image_extraction.py
+++ image_extraction.py
@@ -1,13 +1,19 @@
+import random
+
 import cv2
-
-vidcap = cv2.VideoCapture('ES 19 Hamkam - Rosenborg.mov')
-success, image = vidcap.read()
-count = 0
-
-print(success)
-
-while count < 100:
-    cv2.imwrite("frame%d.jpg" % count, image)  # save frame as JPEG file
+def ya():
+    vidcap = cv2.VideoCapture('ES 19 Hamkam - Rosenborg.mov')
     success, image = vidcap.read()
-    print('Read a new frame: ', success)
-    count += 1
+    count = 0
+
+    print(success)
+
+    while count < 10000:
+        rand = random.randint(0, 10)
+        if (rand == 9):
+            cv2.imwrite("frame%d.jpg" % count, image)  # save frame as JPEG file
+        success, image = vidcap.read()
+        print('Read a new frame: ', success)
+        count += 1
+
+ya()
--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-import ultralytics as ultralytics
-import torch
+import torch.version
+import gc
 from super_gradients.training import models, Trainer
 from ultralytics import NAS, YOLO
-import numpy as np
-from PIL import Image
-import requests
-from io import BytesIO
-import cv2
 
-device = 0 if torch.cuda.is_available() else "cpu"
 
 def define_NAS():
-    yolo_nas_m = models.get("yolo_nas_m", pretrained_weights="coco")
-
+    device = 'cuda' if torch.cuda.is_available() else "cpu"
+    input_video_path = "30Sec.mp4"
+    output_video_path = input_video_path + "-detections.mp4"
+    device = 0
+    yolo_nas_l = models.get("yolo_nas_l", pretrained_weights="coco")
     url = "frame99.jpg"
-    yolo_nas_m.predict(url, conf=0.25).show()
+    torch.cuda.empty_cache()
+    #Using cuda
+    yolo_nas_l.to(device).predict(input_video_path).save(output_video_path)
+    # Not using cuda
+    # yolo_nas_l.predict(input_video_path).save(output_video_path)
 
 
 def define_YOLO():
-    yolov8 = YOLO('yolov8n.pt')
+    ah = torch.cuda.is_available()
+    cuda_id = torch.cuda.current_device()
+    name = torch.cuda.get_device_name(cuda_id)
+    print(name)
+    yolov8 = YOLO('runs/detect/yolov8n_custom10/weights/best.pt')
     CHECKPOINT_DIR = 'checkpoints'
     trainer = Trainer(experiment_name="yolo_player_detectionv1", ckpt_root_dir=CHECKPOINT_DIR)
-    results = yolov8.train(
-        data='yoloTraining/data.yaml',
-        imgsz=640,
-        epochs=10,
-        batch=8,
-        name='yolov8n_custom')
-    url = "frame99.jpg"
-    yolov8.predict(url)
+
+    # results = yolov8.train(
+    #     data='data.yaml',
+    #     imgsz=640,
+    #    epochs=10,
+    #     batch=8,
+    #    name='yolov8n_custom')
+
+    yolov8.predict("frames/frame2.jpg", save=True, conf=0.3)
 
 
-define_YOLO()
+if __name__ == "__main__":
+    define_NAS()
